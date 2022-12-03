06d0e43adde55a8c3a0ca29a6316d4c25a3b7ff8
Jeff Mallozzi <jeffmallozzi@gmail.com>
2022-12-03 13:12:17 -0500

    Day 03 - B

--- day_03.py
+++ day_03.py
@@ -1,7 +1,16 @@
 from string import ascii_lowercase, ascii_uppercase
+from itertools import islice
 
 input_file = "input-03.txt"
 
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while (batch := list(islice(it, n))):
+        yield batch
 
 def bisect(word):
     return word[0:len(word)//2], word[len(word)//2:]
@@ -14,16 +23,11 @@ def main():
     }
 
     with open(input_file, "r") as f:
-        print(
-            sum(
-                [priority[
-                    set(
-                        bisect(line.strip())[0]
-                    ).intersection(set(bisect(line.strip())[1])).pop()
-                ]
-                for line in f.readlines()]
-            )
-            
+        print(sum(
+            [priority[set(lines[0].strip()).intersection(
+                set(lines[1].strip())).intersection(set(lines[2].strip())).pop()]
+            for lines in batched(f, 3)]   
+            )        
         )
     
     
