87c38795eab4564f0ea496f4a51fd0796e9f736f
Jeff Mallozzi <jeffmallozzi@gmail.com>
2022-12-05 21:03:03 -0500

    Day 04 - B

--- day_04.py
+++ day_04.py
@@ -1,7 +1,8 @@
 input_file = "input-04.txt"
 
 def main():
-    count = 0
+    count1 = 0
+    count2 = 0
 
     with open(input_file, "r") as f:
         for line in f:
@@ -10,15 +11,23 @@ def main():
             start2, stop2 = [int(num) for num in elf2.split("-")]
 
             if start1 == start2:
-                count += 1
+                count1 += 1
+                count2 +=1
             elif start1 < start2:
                 if stop2 <= stop1:
-                    count += 1
+                    count1 += 1
+                if start2 <= stop1:
+                    count2 += 1
             else:
                 if stop2 >= stop1:
-                    count += 1
+                    count1 += 1
+                if stop2 >= start1:
+                    count2 += 1
 
-    print("Part 1: ", count)
+            
+
+    print("Part 1: ", count1)
+    print("Part 2: ", count2)
 
 
 if __name__ == "__main__":
