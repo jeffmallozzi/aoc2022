c29700bf9687546d2579d0991ed9d89c7b286b09
Jeff Mallozzi <jeffmallozzi@gmail.com>
2022-12-06 08:45:51 -0500

    Day 06 - B

--- day_06.py
+++ day_06.py
@@ -9,6 +9,11 @@ def main():
             print("Part 1: ", i)
             break
 
+    for i in range(14,len(buffer)+1):
+        if len(set(buffer[i-14:i])) == 14:
+            print("Part 2: ", i)
+            break
+
 
 if __name__ == "__main__":
     main()
