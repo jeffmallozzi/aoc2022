4eef6b30bd0a97f445cfde8162685f7e1443d81b
Jeff Mallozzi <jeffmallozzi@gmail.com>
2022-12-06 08:52:43 -0500

    refactored

--- day_06.py
+++ day_06.py
@@ -1,18 +1,17 @@
 input_file = "input-06.txt"
 
+def unique_sequence(buffer, number):
+    for i in range(number, len(buffer) + 1):
+        if len(set(buffer[i-number:i])) == number:
+            return i
+
+
 def main():
     with open(input_file, "r") as f:
         buffer = f.readline().strip()
 
-    for i in range(4,len(buffer)+1):
-        if len(set(buffer[i-4:i])) == 4:
-            print("Part 1: ", i)
-            break
-
-    for i in range(14,len(buffer)+1):
-        if len(set(buffer[i-14:i])) == 14:
-            print("Part 2: ", i)
-            break
+    print("Part 1: ", unique_sequence(buffer, 4))
+    print("Part 2: ", unique_sequence(buffer, 14))
 
 
 if __name__ == "__main__":
